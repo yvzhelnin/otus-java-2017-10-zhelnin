b25580fc7273dc459c514dd170dcd5a43a6f1f36
Yury Zhelnin <yvzhelnin@gmail.com>
2017-10-14 21:43:56 +0500

    Wrote the basic functional, made it work

--- lesson1/src/main/java/ru/zhelnin/otus/lesson1/Main.java
+++ lesson1/src/main/java/ru/zhelnin/otus/lesson1/Main.java
@@ -1,8 +1,30 @@
 package ru.zhelnin.otus.lesson1;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code herede
+        Options options = new Options();
+
+        Option input = new Option("is", "inputString", true, "input string to print (if there are space chars in the " +
+                "string you have to use quotes)");
+        input.setRequired(true);
+        options.addOption(input);
+
+        CommandLine commandLine = null;
+        try {
+            commandLine = new DefaultParser().parse(options, args);
+        } catch (ParseException e) {
+            System.out.println("Illegal argument!");
+            new HelpFormatter().printHelp("utility-name", options);
+            System.exit(1);
+        }
+        System.out.println(commandLine != null ? commandLine.getOptionValue("inputString") : "Error!");
     }
 }
