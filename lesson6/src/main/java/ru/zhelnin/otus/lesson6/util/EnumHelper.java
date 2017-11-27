7b601e663a79c3c412d0d8c40a65d349011a7ee8
Yury Zhelnin <yvzhelnin@gmail.com>
2017-11-27 21:54:52 +0500

    Changed returning collection type in EnumHelper from List to Set to optimize contains method

--- lesson6/src/main/java/ru/zhelnin/otus/lesson6/util/EnumHelper.java
+++ lesson6/src/main/java/ru/zhelnin/otus/lesson6/util/EnumHelper.java
@@ -2,12 +2,13 @@ package ru.zhelnin.otus.lesson6.util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class EnumHelper<T, R> {
 
-    public List<R> getAvailableValues(Class<T> enumType, Function<T, R> function) {
-        return Arrays.stream(enumType.getEnumConstants()).map(function).collect(Collectors.toList());
+    public Set<R> getAvailableValues(Class<T> enumType, Function<T, R> function) {
+        return Arrays.stream(enumType.getEnumConstants()).map(function).collect(Collectors.toSet());
     }
 }
